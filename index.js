1ebbbf4cf2f6bbcc44fd76077eb19324d4ce53b0
Sarav <sarasoft@live.com>
2022-10-08 00:38:00 +0530

    KubeFerret - Sample Application

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = process.env.appport
+
+if (process.env.ENVIRONMENT='production'){
+    app.use('/', express.static('prod'))
+
+    app.get('/check', (req, res) => {
+        res.send(process.env.welcomemsg)
+})
+}
+else if (process.env.ENVIRONMENT='qa'){
+    app.use('/', express.static('qa'))
+
+    app.get('/check', (req, res) => {
+        res.send(process.env.welcomemsg)
+})
+}
+else if (process.env.ENVIRONMENT='dev'){
+    app.use('/', express.static('dev'))
+
+    app.get('/check', (req, res) => {
+        res.send(process.env.welcomemsg)
+})
+}
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
